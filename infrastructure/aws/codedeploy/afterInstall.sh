505a2e3b66d2013a1689ef7cf32f169a332c6dce
varadjoshi89 <joshi.vara@husky.neu.edu>
2019-11-02 20:51:57 -0400

    circle change

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo systemctl stop tomcat.service
+sudo systemctl stop tomcat
 
-sudo rm -rf /opt/tomcat/webapps/docs  /opt/tomcat/webapps/examples /opt/tomcat/webapps/host-manager  /opt/tomcat/webapps/manager /opt/tomcat/webapps/ROOT
+sudo rm -rf /opt/tomcat/webapps/ROOT
 
 sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
 
@@ -10,4 +10,7 @@ sudo chown tomcat:tomcat /opt/tomcat/webapps/ROOT.war
 sudo rm -rf /opt/tomcat/logs/catalina*
 sudo rm -rf /opt/tomcat/logs/*.log
 sudo rm -rf /opt/tomcat/logs/*.txt
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/tomcat/cloudwatch-config.json -s
+
+sudo systemctl start tomcat
+
+echo $JAVA_OPTS
