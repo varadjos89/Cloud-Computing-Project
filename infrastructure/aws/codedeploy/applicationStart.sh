1f04bf131903f7763ee30dd0a249bcd6909d6148
varadjoshi89 <joshi.vara@husky.neu.edu>
2019-11-06 12:14:22 -0500

    change

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -6,6 +6,6 @@
 #java -jar /opt/tomcat/webapps/demo-0.0.1-SNAPSHOT.war &
 
 nohup java -jar /opt/tomcat/webapps/demo-0.0.1-SNAPSHOT.war > /path/to/log.txt 2>&1 &
-echo $! > /path/to/app/pid.file
+#echo $! > /path/to/app/pid.file
 
 
--- infrastructure/aws/codedeploy/applicationStop.sh
+++ infrastructure/aws/codedeploy/applicationStop.sh
@@ -6,17 +6,17 @@
 # stop tomcat service
 #sudo systemctl stop tomcat.service
 
-#echo "getting the process id"
-#PID=`ps -eaf | grep "java -jar /opt/tomcat/webapps/demo-0.0.1-SNAPSHOT.war" | grep -v grep | awk '{print $2}'`
-#echo "process id not empty ? $PID"
-#if [[ "" !=  "$PID" ]]; then
-#  echo "Found application running on $PID"
-#  echo "killing the process to stop application"
-#  echo "killing $PID"
-#  kill -9 $PID
-#fi
+echo "getting the process id"
+PID=`ps -eaf | grep "java -jar /opt/tomcat/webapps/demo-0.0.1-SNAPSHOT.war" | grep -v grep | awk '{print $2}'`
+echo "process id not empty ? $PID"
+if [[ "" !=  "$PID" ]]; then
+  echo "Found application running on $PID"
+  echo "killing the process to stop application"
+  echo "killing $PID"
+  kill -9 $PID
+fi
 
-kill $(cat /path/to/app/pid.file)
+#kill $(cat /path/to/app/pid.file)
 
 
 
