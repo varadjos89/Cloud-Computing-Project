f89952ec83b964d876ec7e815288acce5a49bc16
varadjoshi89 <joshi.vara@husky.neu.edu>
2019-11-06 17:54:37 -0500

    change

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#sudo cd /opt/tomcat/bin/
-#sudo sh startup.sh
-sudo systemctl start tomcat.service
-#java -jar /opt/tomcat/webapps/demo-0.0.1-SNAPSHOT.war &
+nohup java -jar /opt/tomcat/apache-tomcat-9.0.27/webapps/demo-0.0.1-SNAPSHOT.war &
 
 #nohup java -jar /opt/tomcat/webapps/demo-0.0.1-SNAPSHOT.war > /path/to/log.txt 2>&1 &
 
