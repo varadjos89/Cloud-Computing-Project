0238218c88091f6be68860fe424c81fde5d08c48
varadjoshi89 <joshi.vara@husky.neu.edu>
2019-11-06 19:17:08 -0500

    change

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nohup java -jar /opt/tomcat/apache-tomcat-9.0.27/webapps/demo-0.0.1-SNAPSHOT.war &
+java -jar /opt/tomcat/apache-tomcat-9.0.27/webapps/demo-0.0.1-SNAPSHOT.war &
 
 #nohup java -jar /opt/tomcat/webapps/demo-0.0.1-SNAPSHOT.war > /path/to/log.txt 2>&1 &
 
